9bfed14a867584e8464eab66c44297fc351ded93
wsahn <iwondory@gmail.com>
2017-09-02 22:16:23 +0900

    ''

--- tls_client.go
+++ tls_client.go
@@ -17,6 +17,7 @@ func main() {
 		Certificates:       []tls.Certificate{cert},
 		InsecureSkipVerify: true,
 		MinVersion:         tls.VersionTLS12,
+		MaxVersion:         tls.VersionTLS12,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
