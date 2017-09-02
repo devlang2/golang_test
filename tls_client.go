8de61b64014ce7cec3e4a05bd36b5fa0537ad108
wsahn <iwondory@gmail.com>
2017-09-02 21:41:56 +0900

    ''

--- tls_client.go
+++ tls_client.go
@@ -2,35 +2,42 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
 	"log"
 )
 
 func main() {
-	log.SetFlags(log.Lshortfile)
-
-	conf := &tls.Config{
-	//InsecureSkipVerify: true,
-	}
-
-	conn, err := tls.Dial("tcp", "127.0.0.1:4443", conf)
+	cert, err := tls.LoadX509KeyPair("certs/client.pem", "certs/client.key")
 	if err != nil {
-		log.Println(err)
-		return
+		log.Fatalf("server: loadkeys: %s", err)
+	}
+	config := tls.Config{Certificates: []tls.Certificate{cert}, InsecureSkipVerify: true}
+	conn, err := tls.Dial("tcp", "192.168.0.3:8808", &config)
+	if err != nil {
+		log.Fatalf("client: dial: %s", err)
 	}
 	defer conn.Close()
+	log.Println("client: connected to: ", conn.RemoteAddr())
 
-	n, err := conn.Write([]byte("hello\n"))
-	if err != nil {
-		log.Println(n, err)
-		return
+	state := conn.ConnectionState()
+	for _, v := range state.PeerCertificates {
+		fmt.Println(x509.MarshalPKIXPublicKey(v.PublicKey))
+		fmt.Println(v.Subject)
 	}
+	log.Println("client: handshake: ", state.HandshakeComplete)
+	log.Println("client: mutual: ", state.NegotiatedProtocolIsMutual)
 
-	buf := make([]byte, 100)
-	n, err = conn.Read(buf)
+	message := "Hello\n"
+	n, err := io.WriteString(conn, message)
 	if err != nil {
-		log.Println(n, err)
-		return
+		log.Fatalf("client: write: %s", err)
 	}
+	log.Printf("client: wrote %q (%d bytes)", message, n)
 
-	println(string(buf[:n]))
+	reply := make([]byte, 256)
+	n, err = conn.Read(reply)
+	log.Printf("client: read %q (%d bytes)", string(reply[:n]), n)
+	log.Print("client: exiting")
 }
